15f5caaf32710b2b11ddab8cbd81df29d3e5450f
Pritesh Patel <128021116+codax69@users.noreply.github.com>
2025-09-23 16:38:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000/api/",
+        target: "https://www.vegbazar.cloud/api/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
