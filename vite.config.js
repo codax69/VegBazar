765650dce7a9a1933ad622ab8dc5b517b24ea2f0
codax69 <pritbusiness03@gmail.com>
2026-03-09 16:42:37 +0530

    Remove `loadEnv` and environment variable loading from Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
   return {
     plugins: [react(), tailwindcss()],
 
